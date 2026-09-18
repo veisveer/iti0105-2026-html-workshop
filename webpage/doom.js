704a841f0e1f56b1560ae1c447127f9b50fcd61d
Renee Veisveer <reneve@taltech.ee>
2026-09-18 17:01:46 +0300

    feat: working now

--- webpage/doom.js
+++ webpage/doom.js
@@ -1,4 +1,9 @@
 const hand = document.querySelector('.hand_image');
+const doomMusic = document.getElementById('doom-music');
+const doomGun = document.getElementById('doom-gun');
+
+doomMusic.volume = 0.5;
+doomGun.volume = 1.0;
 
 window.addEventListener('mousemove', (event) => {
 	hand.style.left = `${event.clientX - 25}px`;
@@ -7,6 +12,9 @@ window.addEventListener('mousemove', (event) => {
 
 window.addEventListener('mouseup', async () => {
 	hand.src = "./images/hand_shoot.png";
+	doomGun.currentTime = 0;
+	await doomGun.play().catch(() => {});
+	if (doomMusic.paused) await doomMusic.play().catch(() => {});
 	await new Promise(r => setTimeout(r, 500));
 	hand.src = "./images/hand.png";
 });
